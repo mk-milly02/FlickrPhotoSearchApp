952470fbb47154a6ad08785c174c5b4fb60427d2
Jonas Ababio <jonasagesmilly@outlook.com>
2022-02-11 20:32:19 +0000

    Configure ShellView as RootView

---
+++ PhotoApp.UI/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using PhotoApp.UI.ViewModels;
+using System.Windows;
+
+namespace PhotoApp.UI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ PhotoApp.UI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoApp.UI.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        public ShellViewModel()
+        {
+
+        }
+    }
+}

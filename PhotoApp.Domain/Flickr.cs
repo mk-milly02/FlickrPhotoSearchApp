492eff19d4fcc47ac63270fcaec4350af2330e55
Jonas Ababio <jonasagesmilly@outlook.com>
2022-02-12 22:17:09 +0000

    Create GetFeedAsync method for Flickr class

---
+++ PhotoApp.Domain/Flickr.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using PhotoApp.Domain.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PhotoApp.Domain
+{
+    public static class Flickr
+    {
+        public static async Task<Feed> GetFeedAsync(string search)
+        {
+            string uri = $"https://www.flickr.com/services/feeds/photos_public.gne?format=json&tags={search}";
+
+            HttpClient client = new();
+
+            var response = await client.GetStringAsync(uri);
+
+            Feed feed = JsonConvert.DeserializeObject<Feed>(response);
+
+            return feed;
+        }
+    }
+}

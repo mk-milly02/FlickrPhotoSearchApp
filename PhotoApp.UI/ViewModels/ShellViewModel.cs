b332164d9037e57e95f4fe5e753bd60895a3089c
Jonas Ababio <jonasagesmilly@outlook.com>
2022-02-14 16:41:08 +0000

    Display title of feed

--- PhotoApp.UI/ViewModels/ShellViewModel.cs
+++ PhotoApp.UI/ViewModels/ShellViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Caliburn.Micro;
+using PhotoApp.Domain;
+using PhotoApp.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +24,36 @@ namespace PhotoApp.UI.ViewModels
             {
                 keyword = value;
                 NotifyOfPropertyChange(() => Keyword);
+                NotifyOfPropertyChange(() => CanSearchAsync);
+            }
+        }
+
+        public Feed FlickrFeed { get; set; }
+
+        private string title;
+
+        public string FeedTitle
+        {
+            get { return title; }
+            set 
+            {
+                title = value;
+                NotifyOfPropertyChange(() => FeedTitle);
+            }
+        }
+
+        public bool CanSearchAsync
+        {
+            get 
+            {
+                bool output = true;
+
+                if (string.IsNullOrWhiteSpace(Keyword))
+                {
+                    output = false; 
+                }
+
+                return output;
             }
         }
 
@@ -31,7 +63,8 @@ namespace PhotoApp.UI.ViewModels
 
         public async Task SearchAsync()
         {
-
+            FlickrFeed = await Flickr.GetFeedAsync(Keyword);
+            FeedTitle = FlickrFeed.Title;
         }
 
         #endregion

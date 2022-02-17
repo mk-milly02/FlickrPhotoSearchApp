1650ba63597540e48132deec299f53e035968864
Jonas Ababio <jonasagesmilly@outlook.com>
2022-02-17 21:42:33 +0000

    Add appsettings

---
+++ PhotoApp.Domain/Twitter.cs
@@ -0,0 +1,26 @@
+﻿using PhotoApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoApp.Domain
+{
+    public class Twitter
+    {
+        public async Task<TwitterApiResponse> GetTweetsAsync(string search)
+        {
+            string uri = $"https://api.twitter.com/2/tweets/search/recent?max_results=60&expansions=author_id&user.fields=created_at,description&query={search}";
+            
+            HttpClient client = new();
+
+            var response = await client.GetAsync(uri);
+
+            response.Headers.Add("Authorization", "application/json");
+
+
+        }
+    }
+}

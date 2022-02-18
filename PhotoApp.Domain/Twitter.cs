90cae49d3a947b72adc723fa08ef3b344eca17ce
Jonas Ababio <jonasagesmilly@outlook.com>
2022-02-18 08:01:50 +0000

    Add GetTweetsAsync to Twitter class

--- PhotoApp.Domain/Twitter.cs
+++ PhotoApp.Domain/Twitter.cs
@@ -1,26 +1,36 @@
-﻿using PhotoApp.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using PhotoApp.Domain.Models;
+using System.IO;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace PhotoApp.Domain
 {
     public class Twitter
     {
-        public async Task<TwitterApiResponse> GetTweetsAsync(string search)
+        private static string GetBearerToken()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("Appsettings.json").Build();
+
+            return configuration.GetSection("bearer_token").Value;
+        }
+
+        public static async Task<TwitterApiResponse> GetTweetsAsync(string search)
         {
             string uri = $"https://api.twitter.com/2/tweets/search/recent?max_results=60&expansions=author_id&user.fields=created_at,description&query={search}";
             
             HttpClient client = new();
 
-            var response = await client.GetAsync(uri);
+            client.DefaultRequestHeaders.Add("Authorization", GetBearerToken());
 
-            response.Headers.Add("Authorization", "application/json");
+            var response = await client.GetStringAsync(uri);
 
+            TwitterApiResponse responseBody = JsonConvert.DeserializeObject<TwitterApiResponse>(response);
 
+            return responseBody;
         }
     }
 }
--- PhotoApp.UI/Bootstrapper.cs
+++ PhotoApp.UI/Bootstrapper.cs
@@ -1,9 +1,5 @@
 ﻿using Caliburn.Micro;
-using Microsoft.Extensions.Configuration;
 using PhotoApp.UI.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Windows;
 
 namespace PhotoApp.UI
@@ -20,37 +16,6 @@ namespace PhotoApp.UI
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
-        }
-
-        public IConfiguration AddConfiguration()
-        {
-            IConfiguration configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json").Build();
-
-            return configuration;
-        }
-
-        protected override void Configure()
-        {
-            container.Instance(container);
-
-            container.RegisterInstance(typeof(IConfiguration), "IConfiguration", AddConfiguration());
-        }
-
-        protected override void BuildUp(object instance)
-        {
-            container.BuildUp(instance);
-        }
-
-        protected override IEnumerable<object> GetAllInstances(Type service)
-        {
-            return container.GetAllInstances(service);
-        }
-
-        protected override object GetInstance(Type service, string key)
-        {
-            return container.GetInstance(service, key);
         }
     }
 }
